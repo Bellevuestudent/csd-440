9006bfcd9ed4543254b5f3b0b6eb26273d30ab2e
Bellevuestudent <pmoracchini@my365.bellevue.edu>
2026-08-23 23:39:28 -0500

    Loading assignment 3.2"

---
+++ module-3/PatriceTable3Function.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * PatriceTable3Function.php
+ *
+ * This file contains a function that adds two numbers together.
+ * and is included in the main file (PatriceTable3.php).
+ */
+
+function addNumbers(int $number1, int $number2): int{
+  
+  $sum = $number1 + $number2;
+  return $sum; 
+}
+?>
